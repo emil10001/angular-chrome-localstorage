05b5cc2b863ed65e5f0a1f697a3f3a17fd68ec97
E John Feig <ejohn@feigdev.com>
2013-12-08 13:58:04 -0800

    allow for a unique emit string

--- localwrapper.js
+++ localwrapper.js
@@ -6,48 +6,44 @@
 angular.module('angular-chrome-localstorage', [ 'ngResource' ])
     .service('LocalWrapper', function ($rootScope) {
 
-        this.GET = 'get';
-        this.SET = 'set';
-        this.REMOVE = 'remove';
-
-        this.get = function (key) {
+        this.get = function (key, EMIT_STR) {
             if (!!chrome && !!chrome.storage) {
                 chrome.storage.sync.get(key, function(data){
-                    console.log(this.GET,data);
-                    $rootScope.$emit(this.GET, data);
+                    console.log(EMIT_STR,data);
+                    $rootScope.$emit(EMIT_STR, data);
                 });
             } else {
-                $rootScope.$emit(this.GET, localStorage[key]);
+                $rootScope.$emit(EMIT_STR, localStorage[key]);
             }
         };
 
-        this.set = function (key, val) {
+        this.set = function (key, val, EMIT_STR) {
             var newSomething = {};
             newSomething[key] = val;
-            console.log(this.SET, newSomething);
+            console.log(EMIT_STR, newSomething);
 
             if (!!chrome && !!chrome.storage) {
                 chrome.storage.sync.set(newSomething, function(){
-                    $rootScope.$emit(this.SET);
+                    $rootScope.$emit(EMIT_STR);
                 });
             } else {
                 localStorage[key] = val;
-                $rootScope.$emit(this.SET);
+                $rootScope.$emit(EMIT_STR);
             }
         };
 
-        this.remove = function (key) {
+        this.remove = function (key, EMIT_STR) {
             var newSomething = {};
             newSomething[key] = "";
-            console.log(this.REMOVE, newSomething);
+            console.log(EMIT_STR, newSomething);
 
             if (!!chrome && !!chrome.storage) {
                 chrome.storage.sync.set(newSomething, function(){
-                    $rootScope.$emit(this.REMOVE);
+                    $rootScope.$emit(EMIT_STR);
                 });
             } else {
                 localStorage[key] = val;
-                $rootScope.$emit(this.REMOVE);
+                $rootScope.$emit(EMIT_STR);
             }
         }
 
